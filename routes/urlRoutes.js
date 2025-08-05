c5e48e2539d5949d9eb29a7130ac45e32656d413
nethajisaineeraj <saineerajmahanthi@gmail.com>
2025-08-05 13:09:09 +0530

    Delete routes directory

--- routes/urlRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {shortenUrl,redirectUrl} from "../controllers/urlController.js";
-
-const router=express.Router();
-router.post("/shorten",shortenUrl);
-router.get("/:shortId",redirectUrl);
-
-export default router;
